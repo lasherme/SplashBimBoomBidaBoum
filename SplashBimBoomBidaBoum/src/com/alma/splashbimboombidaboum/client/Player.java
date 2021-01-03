f119f28dd4853e1d1bc42e0ef628e839b238e0c4
lasherme <loic.lasherme@outlook.fr>
2021-01-03 09:13:01 +0100

    Ajout de la javadoc

--- SplashBimBoomBidaBoum/src/com/alma/splashbimboombidaboum/client/Player.java
+++ SplashBimBoomBidaBoum/src/com/alma/splashbimboombidaboum/client/Player.java
@@ -7,6 +7,7 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 import com.alma.splashbimboombidaboum.client.controller.WaitingRoomController;
+import com.alma.splashbimboombidaboum.server.EntityInterface;
 import com.alma.splashbimboombidaboum.server.RoomInterface;
 import com.alma.splashbimboombidaboum.server.RoomReservationInterface;
 import com.alma.splashbimboombidaboum.utility.Address;
@@ -14,6 +15,19 @@ import com.alma.splashbimboombidaboum.utility.Address;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 
+/**
+ * <b>Classe représentant un joueur.</b>
+ * <p>
+ * Elle peut être aussi utilisée comme objet distant.
+ * </p>
+ * <p>
+ * Voir {@link PlayerInterface} pour plus de renseignements.
+ * </p>
+ * 
+ * @see Address
+ * @author Éloi Filaudeau, Louis Boursier, Nicolas Hawa, Loïc Lasherme
+ * @version 1.0
+ */
 public class Player extends UnicastRemoteObject implements PlayerInterface, Address {
 	private String name;
 	private boolean ready = false;
@@ -26,88 +40,155 @@ public class Player extends UnicastRemoteObject implements PlayerInterface, Addr
 	private PlayerEntityInterface playerEntity;
 	private ObservableDataInterface observablePlayers;
 
+	/**
+	 * Constructeur renseignant le nom du joueur.
+	 * 
+	 * <p>
+	 * Initialise aussi l'entité du joueur.
+	 * </p>
+	 * 
+	 * @param name Le nom du joueur.
+	 * @throws RemoteException
+	 */
 	public Player(String name) throws RemoteException {
 		this.name = name;
 		this.playerEntity = new PlayerEntity();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public String getName() throws RemoteException {
 		return this.name;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public boolean getState() throws RemoteException {
 		return this.ready;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public String getColor() throws RemoteException {
 		return this.colorString;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public Scene getScene() throws RemoteException {
 		return this.scene;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public WaitingRoomController getWaitingRoom() throws RemoteException {
 		return this.waitingRoom;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public RoomReservationInterface getServer() throws RemoteException {
 		return this.server;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public RoomInterface getRoom() throws RemoteException {
 		return this.room;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public PlayerEntityInterface getPlayerEntity() throws RemoteException {
 		return this.playerEntity;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public ObservableDataInterface getObservablePlayers() throws RemoteException {
 		return this.observablePlayers;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public void setName(String name) throws RemoteException {
 		this.name = name;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public void setState(boolean ready) throws RemoteException {
 		this.ready = ready;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public void setColor(String color) throws RemoteException {
 		this.color = Color.web(color);
 		this.colorString = color;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public void setScene(Scene scene) throws RemoteException {
 		this.scene = scene;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public void setWaitingRoom(WaitingRoomController waitingRoom) throws RemoteException {
 		this.waitingRoom = waitingRoom;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
+	public void setRoom(RoomInterface room) throws RemoteException {
+		this.room = room;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void createObservablePlayers() throws RemoteException {
+		this.observablePlayers = new ObservableData();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
 	public RoomReservationInterface connection() throws MalformedURLException, RemoteException, NotBoundException {
 		this.server = (RoomReservationInterface) Naming.lookup("//" + PREADRRESS + ":" + PORT + "/" + POSTADDRESS);
 
 		return this.server;
 	}
 
-	public void setRoom(RoomInterface room) throws RemoteException {
-		this.room = room;
-	}
-
-	public void createObservablePlayers() throws RemoteException {
-		this.observablePlayers = new ObservableData();
-	}
-	
+	/**
+	 * {@inheritDoc}
+	 */
 	public RoomInterface roomConnection() throws RemoteException {
 		this.room = server.getRoom(this);
 
 		return this.room;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public RoomInterface roomConnection(String id) throws RemoteException {
 		this.room = server.getRoom(this, id);
 
--- SplashBimBoomBidaBoum/src/com/alma/splashbimboombidaboum/client/PlayerInterface.java
+++ SplashBimBoomBidaBoum/src/com/alma/splashbimboombidaboum/client/PlayerInterface.java
@@ -11,43 +11,196 @@ import com.alma.splashbimboombidaboum.server.RoomReservationInterface;
 
 import javafx.scene.Scene;
 
+/**
+ * <b>Interface représentant un joueur.</b>
+ * <p>
+ * Elle peut être utilisée comme objet distant.
+ * </p>
+ * <p>
+ * Un joueur est caractérisé par les informations suivantes :
+ * <ul>
+ * <li>Un nom</li>
+ * <li>Un boolean qui indique si le joueur est prêt</li>
+ * <li>Une couleur</li>
+ * <li>Le code hexadécimal de la couleur</li>
+ * <li>Une scène de JavaFX</li>
+ * <li>Un controleur de la salle d'attente</li>
+ * <li>La référence vers le serveur</li>
+ * <li>La référence vers le lobby</li>
+ * <li>Une entité joueur</li>
+ * <li>Un objet qui encapsule les observeurs de JavaFX</li>
+ * </ul>
+ * </p>
+ * 
+ * @see ObservableDataInterface
+ * @see PlayerEntityInterface
+ * @see RoomInterface
+ * @see RoomReservationInterface
+ * @see WaitingRoomController
+ * @author Éloi Filaudeau, Louis Boursier, Nicolas Hawa, Loïc Lasherme
+ * @version 1.0
+ */
 public interface PlayerInterface extends Remote {
-
+	/**
+	 * Retourne le nom du joueur.
+	 * 
+	 * @return Le nom du joueur.
+	 * @throws RemoteException
+	 */
 	public String getName() throws RemoteException;
 
+	/**
+	 * Retourne l'état du joueur (prêt ou non).
+	 * 
+	 * @return L'état du joueur
+	 * @throws RemoteException
+	 */
 	public boolean getState() throws RemoteException;
 
+	/**
+	 * Retourne le code hexadécimal de la couleur du joueur.
+	 * 
+	 * @return Le code hexadécimal de la couleur.
+	 * @throws RemoteException
+	 */
 	public String getColor() throws RemoteException;
 
+	/**
+	 * Retourne la scène du joueur (scène de JavaFX).
+	 * 
+	 * @return La scène du joueur.
+	 * @throws RemoteException
+	 */
 	public Scene getScene() throws RemoteException;
 
+	/**
+	 * Retourne le controleur de la salle d'attente du joueur.
+	 * 
+	 * @return La salle d'attente du joueur.
+	 * @throws RemoteException
+	 * @see WaitingRoomController
+	 */
 	public WaitingRoomController getWaitingRoom() throws RemoteException;
 
+	/**
+	 * Retourne le serveur auquel est connecté le joueur.
+	 * 
+	 * @return Le serveur du joueur.
+	 * @throws RemoteException
+	 * @see RoomReservationInterface
+	 */
 	public RoomReservationInterface getServer() throws RemoteException;
 
+	/**
+	 * Retourne le lobby auquel est rattaché le joueur.
+	 * 
+	 * @return Le lobby du joueur.
+	 * @throws RemoteException
+	 * @see RoomInterface
+	 */
 	public RoomInterface getRoom() throws RemoteException;
 
+	/**
+	 * Retourne l'entité joueur du joueur.
+	 * 
+	 * @return L'entité joueur du joueur.
+	 * @throws RemoteException
+	 * @see PlayerEntityInterface
+	 */
 	public PlayerEntityInterface getPlayerEntity() throws RemoteException;
 
+	/**
+	 * Retourne les observeurs du joueur (observeurs de JavaFX).
+	 * 
+	 * @return Les observeurs du joueur.
+	 * @throws RemoteException
+	 * @see ObservableDataInterface
+	 */
 	public ObservableDataInterface getObservablePlayers() throws RemoteException;
 
+	/**
+	 * Met à jour le nom du joueur.
+	 * 
+	 * @param name Le nouveau nom du joueur.
+	 * @throws RemoteException
+	 */
 	public void setName(String name) throws RemoteException;
 
+	/**
+	 * Met à jour l'état du joueur.
+	 * 
+	 * @param ready Le nouvel état du joueur.
+	 * @throws RemoteException
+	 */
 	public void setState(boolean ready) throws RemoteException;
 
+	/**
+	 * Met à jour la couleur et son code hexadécimal du joueur.
+	 * 
+	 * @param color Le nouveau code hexadécimal de la couleur du joueur.
+	 * @throws RemoteException
+	 */
 	public void setColor(String color) throws RemoteException;
 
+	/**
+	 * Met à jour la scène du joueur.
+	 * 
+	 * @param scene La nouvel scène du joueur.
+	 * @throws RemoteException
+	 */
 	public void setScene(Scene scene) throws RemoteException;
 
+	/**
+	 * Met à jour la salle d'attente du joueur.
+	 * 
+	 * @param waitingRoom La nouvelle salle d'attente du joueur.
+	 * @throws RemoteException
+	 * @see WaitingRoomController
+	 */
 	public void setWaitingRoom(WaitingRoomController waitingRoom) throws RemoteException;
 
-	public RoomReservationInterface connection() throws MalformedURLException, RemoteException, NotBoundException;
-
+	/**
+	 * Met à jour le lobby du joueur.
+	 * 
+	 * @param room Le nouveau lobby du joueur.
+	 * @throws RemoteException
+	 * @see RoomInterface
+	 */
 	public void setRoom(RoomInterface room) throws RemoteException;
 
+	/**
+	 * Créé un objet encapsulant les observeurs
+	 * 
+	 * @throws RemoteException
+	 */
 	public void createObservablePlayers() throws RemoteException;
 
+	/**
+	 * Permet de connecter le joueur au serveur.
+	 * 
+	 * @return Le serveur auquel le joueur est connecté
+	 * @throws MalformedURLException
+	 * @throws RemoteException
+	 * @throws NotBoundException
+	 */
+	public RoomReservationInterface connection() throws MalformedURLException, RemoteException, NotBoundException;
+
+	/**
+	 * Permet de se connecter à un lobby aléatoire.
+	 * 
+	 * @return Le lobby du joueur.
+	 * @throws RemoteException
+	 * @see RoomInterface
+	 */
 	public RoomInterface roomConnection() throws RemoteException;
 
+	/**
+	 * Permet de se connecter à un lobby dont l'adresse est passé en paramètre.
+	 * 
+	 * @param id L'adresse du lobby.
+	 * @return Le lobby du joueur.
+	 * @throws RemoteException
+	 * @see RoomInterface
+	 */
 	public RoomInterface roomConnection(String id) throws RemoteException;
 }
